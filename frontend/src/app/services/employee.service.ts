67ea37b322b24d5d1c805b51208bf0d1127872f7
Kevin Bueno <kean.solano@gmail.com>
2019-01-26 14:45:27 -0500

    Frontend
    Creacion del fronted con Angular

---
+++ frontend/src/app/services/employee.service.ts
@@ -0,0 +1,33 @@
+import { Employee } from './../models/employee';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class EmployeeService {
+
+  selectedEmployee: Employee;
+  employees: Employee[];
+  readonly URL_API = 'http://localhost:3000/api/employees';
+
+constructor( private http: HttpClient ) {
+  this.selectedEmployee = new Employee();
+}
+
+getEmployees() {
+  return this.http.get(this.URL_API);
+}
+
+postEmployee(employee: Employee) {
+  console.log(employee);
+  return this.http.post(this.URL_API, employee);
+}
+
+putEmployee(employee: Employee) {
+  return this.http.put(this.URL_API + `/${employee._id}`, employee);
+}
+
+deleteEmployee(_id: string) {
+  return this.http.delete(this.URL_API + `/${_id}`);
+}
+
+}
